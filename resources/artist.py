b8b8111c050590cbe86f93cd20c8510c779bcec6
Alessandro Henrique <alessandrohenriqueho@gmail.com>
2022-05-14 00:16:40 -0300

    Implementa endpoint pra caso artista exista no dynamodb, caso contrário, ele é criado

--- resources/artist.py
+++ resources/artist.py
@@ -1,6 +1,21 @@
+from flask import request, abort
 from flask_restful import Resource
+from ..common.database import Artist
 
+
+db = Artist()
 
 class Artist(Resource):
     def get(self):
-        return {'message': ''}
+        name = request.args.get('q', None)
+        artist = db.get_artist(name)
+        if artist:
+            return artist
+        else:
+            response = db.create_artist(name)
+            if (response['ResponseMetadata']['HTTPStatusCode'] == 200):
+                return {
+                    'success': True,
+                    'msg': 'Added successfully',
+                }
+            abort(400)
